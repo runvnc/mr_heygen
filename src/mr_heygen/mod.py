070e46152f799a174f7f00af336944a7d8a85f06
Jason Livesay <runvnc@gmail.com>
2025-02-16 06:23:50 -0600

    update

--- src/mr_heygen/mod.py
+++ src/mr_heygen/mod.py
@@ -3,9 +3,4 @@ import json
 import os
 import asyncio
 
-@command()
-async def get_object_images(abs_image_file_path: str, class_names: str, context=None):
 
-if __name__ == "__main__":
-    asyncio.run(get_object_images("baseballcards.jpg", "baseball card, trading card, card"))
-    
