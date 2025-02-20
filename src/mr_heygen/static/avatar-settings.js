65e926e25c7b1469feaa27f6cfc995613e3aa4a0
Jason Livesay <runvnc@gmail.com>
2025-02-20 08:03:20 -0600

    update

--- src/mr_heygen/static/avatar-settings.js
+++ src/mr_heygen/static/avatar-settings.js
@@ -184,11 +184,6 @@ class AvatarSettings extends BaseEl {
           }
         }
       };
-      this.settings.voice.rate *= 1.0;
-      this.settings.scale *= 1.0;
-      this.settings.voice.elevenlabs_settings.stability *= 1.0;
-      this.settings.voice.elevenlabs_settings.similarity_boost *= 1.0;
-      this.settings.voice.elevenlabs_settings.style *= 1;
       return;
     }
 
@@ -203,6 +198,11 @@ class AvatarSettings extends BaseEl {
     
     try {
       this.loading = true;
+      this.settings.voice.rate *= 1.0;
+      this.settings.scale *= 1.0;
+      this.settings.voice.elevenlabs_settings.stability *= 1.0;
+      this.settings.voice.elevenlabs_settings.similarity_boost *= 1.0;
+      this.settings.voice.elevenlabs_settings.style *= 1;
       
       const response = await fetch(`/heygen/avatarsettings/${this.persona}`, {
         method: 'POST',
