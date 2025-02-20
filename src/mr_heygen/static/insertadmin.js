808d1ea985fa70aa66eab9f59535ce39484fb2bb
Jason Livesay <runvnc@gmail.com>
2025-02-19 21:53:08 -0600

    update

--- src/mr_heygen/static/insertadmin.js
+++ src/mr_heygen/static/insertadmin.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   setTimeout(function() {
     const personaEditor = document.querySelector('persona-editor')
     console.log({personaEditor})
-    const savebtn = personaEditor.shadowRoot.querySelector('#save-persona')[0]
+    const savebtn = personaEditor.shadowRoot.querySelectorAll('#save-persona')[0]
     console.log({savebtn})
     const heygenAdmin = document.createElement('avatar-settings')
     console.log({heygenAdmin})
